3918b27b9a102ca09779fafae695765c16e0e655
Ajiri Unuafe <aj4short@rocketmail.com>
2023-02-08 11:29:47 +0100

    create function for random computer choice

---
+++ file.js
@@ -0,0 +1,13 @@
+     function getComputerChoice(){
+            let random = Math.floor(Math.random() * 3)
+             if(random === 0){
+                 console.log('Rock')
+                return 'Rock'
+            }  else if(random === 1){
+                 console.log('Paper')
+                return 'Paper'
+            }  else if(random === 2){
+                console.log('Scissors')
+                return 'Scissors'               
+            } 
+        }
